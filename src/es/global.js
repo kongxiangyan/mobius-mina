e7b16a83822aecf273587374a5c50384bdfe82a0
kongxiangyan <kcigaret@outlook.com>
2021-07-09 13:52:13 +0800

    feat: add enterOptionsRD

--- src/es/global.js
+++ src/es/global.js
@@ -8,6 +8,7 @@ import {
   // @refer: https://developers.weixin.qq.com/miniprogram/dev/reference/api/getApp.html
   // 在 App 注册之前就定义好 globalData 和 setGlobalData
   // 当 App 注册的时候，预先定义的 globalData 和 setGlobalData 会被合并到 App Instance 中
+  // ! 务必在 App(appOptions) 之前执行
   const app = getApp({ allowDefault: true })
   app.globalData = {}
   app.setGlobalData = data => {
@@ -23,4 +24,8 @@ import {
 
 export const wxRD = replayWithLatest(1, Data.of(wx))
 // App 正式注册的时候会更新 appRD 的值（App Instance）
+// 在 App 正式注册之前，appRD 的值是 App 默认实现
+// 约定使用 app.setGlobalData 来添加数据
 export const appRD = replayWithLatest(1, Data.of(getApp({ allowDefault: true })))
+
+export const enterOptionsRD = replayWithLatest(1, Data.of(wx.getEnterOptionsSync()))
