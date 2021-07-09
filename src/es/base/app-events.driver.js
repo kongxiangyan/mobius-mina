18d9439760ab2391ec67e6a9d356447cd53d5c46
kongxiangyan <kcigaret@outlook.com>
2021-07-10 02:03:10 +0800

    docs: add refers

--- src/es/base/app-events.driver.js
+++ src/es/base/app-events.driver.js
@@ -4,6 +4,8 @@ import {
   useGeneralDriver
 } from '../../libs/mobius-utils.js'
 
+// 使用场景：独立分包
+//  -> @refer: https://developers.weixin.qq.com/miniprogram/dev/framework/subpackages/independent.html
 export const appEventsDriver = options => {
   const errorRD = replayWithLatest(1, Data.empty())
   const pageNotFoundRD = replayWithLatest(1, Data.empty())
@@ -14,27 +16,35 @@ export const appEventsDriver = options => {
   const appShowRD = replayWithLatest(1, Data.empty())
   const appHideRD = replayWithLatest(1, Data.empty())
 
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onError.html
   wx.onError(error => {
     errorRD.mutate(() => error)
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onPageNotFound.html
   wx.onPageNotFound(res => {
     pageNotFoundRD.mutate(() => res)
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onUnhandledRejection.html
   wx.onUnhandledRejection(res => {
     unhandledRejectionRD.mutate(() => res)
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onThemeChange.html
   wx.onThemeChange(res => {
     themeChangeRD.mutate(() => res)
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onAudioInterruptionBegin.html
   wx.onAudioInterruptionBegin(() => {
     audioInterruptionBeginRD.mutate(() => ({}))
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onAudioInterruptionEnd.html
   wx.onAudioInterruptionEnd(() => {
     audioInterruptionEndRD.mutate(() => ({}))
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onAppShow.html
   wx.onAppShow(options => {
     appShowRD.mutate(() => options)
   })
+  // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/base/app/app-event/wx.onAppHide.html
   wx.onAppHide(() => {
     appHideRD.mutate(() => ({}))
   })
