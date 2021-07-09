85d7d8f5827c0e70c7faca1e50d3c1db68645b23
kongxiangyan <kcigaret@outlook.com>
2021-07-09 18:23:29 +0800

    feat: clipboardDriver

---
+++ src/es/clipboard.driver.js
@@ -0,0 +1,51 @@
+import {
+  Data,
+  replayWithLatest,
+  useGeneralDriver
+} from '../libs/mobius-utils.js'
+
+export const clipboardDriver = options => {
+  const { autoGet = false } = options
+
+  const getClipboardD = Data.empty()
+  const setClipboardRD = replayWithLatest(1, Data.empty())
+  const clipboardRD = replayWithLatest(1, Data.empty())
+
+  const triggerGetClipboard = () => {
+    getClipboardD.triggerValue({})
+  }
+  getClipboardD.subscribeValue(() => {
+    // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/device/clipboard/wx.getClipboardData.html
+    wx.getClipboardData({
+      success (res) {
+        clipboardRD.mutate(() => res.data)
+      }
+    })
+  })
+  setClipboardRD.subscribeValue(({ data }) => {
+    // @refer: https://developers.weixin.qq.com/miniprogram/dev/api/device/clipboard/wx.setClipboardData.html
+    wx.setClipboardData({
+      data: data,
+      complete: (res) => {
+        // 无论剪贴板设置成功与否都执行
+        triggerGetClipboard()
+      }
+    })
+  })
+
+  if (autoGet) {
+    triggerGetClipboard()
+  }
+
+  return {
+    inputs: {
+      getClipboard: getClipboardD,
+      setClipboard: setClipboardRD
+    },
+    outputs: {
+      clipboard: clipboardRD
+    }
+  }
+}
+
+export const useClipboardDriver = useGeneralDriver(clipboardDriver)
--- src/es/index.js
+++ src/es/index.js
@@ -5,3 +5,4 @@ export * from './page.driver.js'
 
 export * from './theme.driver.js'
 export * from './supported-materials.driver.js'
+export * from './clipboard.driver.js'
